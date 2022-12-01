72d66926da910a3408f93cd31a3c894f9eac12ce
Radiant690 <venkatasaikadamati@gmail.com>
2022-12-01 23:57:59 +0530

    created flutter project flutter_todolist_application

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget{
+  // I now need to have 2 things : constructor & function :: build
+  const MyApp({Key? key}): super(key: key);
+
+  @override
+  Widget build(BuildContext context){
+
+  }
+}
